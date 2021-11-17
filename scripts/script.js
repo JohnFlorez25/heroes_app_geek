9a1ac3686280be2677fd695d0becc5cd9d3f6ac3
John Faber Flórez <john.florez25@gmail.com>
2021-11-17 18:00:01 -0500

    Cargando las imagenes en el dom haciendo uso de fragment y cloneNode

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,38 @@
+//Importar el archivo data.js
 
+import { data }  from '../data/data.js';
+
+console.log(data);
+
+//zona de variables capturas desde el html
+const templateCard = document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
+
+//El evento DOMContentLoaded
+document.addEventListener('DOMContentLoaded', () => {
+
+    //invocar a la funcion para cargar las imagenes en el DOM (loadImagesInDOM)
+    //y le envíamos el parametro de data
+    loadImageInDOM(data);
+});
+
+const loadImageInDOM = data => {
+
+    //capturas uno a uno los elementos que se encuentran en el arreglo de objetos "data"
+    data.forEach( heroe => {
+        
+        templateCard.querySelector('h5').textContent = heroe.superhero;
+        templateCard.querySelector('img').setAttribute('src', heroe.image);
+        templateCard.querySelector('img').dataset.id = heroe.id;
+
+        //cloneNode
+        const clone = templateCard.cloneNode(true);
+        //adicionamos los clones al fragment
+        fragment.appendChild(clone)
+    })
+    //adicionar el fragmen a nuestros ITEMS
+    items.appendChild(fragment);
+}
 
 
 
